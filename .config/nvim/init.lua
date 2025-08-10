3e532706bd7dcf6e9ca378c2b3432e1a9b683663
Danny Kan <danny_kn@proton.me>
2025-08-10 18:18:08 -0400

    dev: refactored a personal neovim configuration.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,33 +1,5 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.opt.signcolumn = "yes"
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = false
-
-vim.opt.wrap = false
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.swapfile = false
-
-vim.opt.termguicolors = true
-vim.opt.winborder = "rounded"
-
-vim.opt.list = true
-vim.opt.listchars = { tab = ">-", trail = "-" }
+require("core.options")
+require("core.keymaps")
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	pattern = { "*" },
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,27 @@
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = false
+
+vim.opt.wrap = false
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.swapfile = false
+
+vim.opt.termguicolors = true
+vim.opt.winborder = "rounded"
+
+vim.opt.list = true
+vim.opt.listchars = { tab = ">-", trail = "-" }
