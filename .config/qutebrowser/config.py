4e90cd1bd94ff081e1df7149f70a644c35e4224f
Danny Kan <danny_kn@proton.me>
2025-08-14 02:13:51 -0400

    dev: refactored a qutebrowser configuration again.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,27 +1,10 @@
-# import catppuccin
-# import os
-# from urllib.request import urlopen
-
-config.load_autoconfig()
-
-# if not os.path.exists(config.configdir / "theme.py"):
-    # theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
-    # with urlopen(theme) as themehtml:
-        # with open(config.configdir / "theme.py", "a") as file:
-            # file.writelines(themehtml.read().decode("utf-8"))
-
-# if os.path.exists(config.configdir / "theme.py"):
-    # import theme
-    # theme.setup(c, "mocha", True)
-    # theme.setup(c, "macchiato", True)
-    # theme.setup(c, "frappe", True)
-    # theme.setup(c, "latte", True)
-
 # base16-qutebrowser (https://github.com/theova/base16-qutebrowser)
 # Scheme name: Grayscale Dark
 # Scheme author: Alexandre Gavioli (https://github.com/Alexx2/)
 # Template author: theova and Daniel Mulford
 # Commentary: Tinted Theming: (https://github.com/tinted-theming)
+
+config.load_autoconfig()
 
 base00 = "#101010"
 base01 = "#252525"
